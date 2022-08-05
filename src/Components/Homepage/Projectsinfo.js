328b79e8aef25d93a37aa65bbebd74076ca1a156
ruckyTheWebDesigner <93795260+ruckyTheWebDesigner@users.noreply.github.com>
2022-08-05 22:12:40 +0300

    Added Shortly Url App to project list

--- src/Components/Homepage/Projectsinfo.js
+++ src/Components/Homepage/Projectsinfo.js
@@ -1,11 +1,29 @@
 const Projectsinfo = [
+        {
+    name: "Shortly Url",
+    id: 1,
+
+    description:
+      "Bored of trying to remember websites with long url? Here is a app that can help you to shorten any website url with just a button click.",
+    url: "https://url-api-sigma.vercel.app/",
+            github: 'https://github.com/ruckyTheWebDesigner/url-api',
+    with: {
+      1: "React",
+      2: "API",
+      3: "React-query",
+      4: "Material UI",
+    },
+  },
+    
+    
     {
     name: "Where in the world",
-    id: 1,
+    id: 2,
 
     description:
       "This is a app that displays all countries with infos, Users can search by country name or select filter, I got the design inspiration from frontend mentor challenges.",
     url: "https://rest-countries-lfur8553i-ruckythewebdesigner.vercel.app/",
+        github: 'https://github.com/ruckyTheWebDesigner/rest-countries',
     with: {
       1: "React",
       2: "API",
@@ -18,12 +36,13 @@ const Projectsinfo = [
   
   {
     name: "HSA news app",
-    id: 2,
+    id: 3,
 
     description:
       "HSA is a modern, easy to use, and easy to navigate news app. It enables readers to search for and read articles in a split-second. The app uses modern Web technologies like React, Redux, and API Fetch to build it's backend and frontend. ",
     github: "https://github.com/ruckyTheWebDesigner/hsanews-app",
     url: "https://hsanews-app.vercel.app/",
+      github: '',
     with: {
       1: "React",
       2: "API",
@@ -34,7 +53,7 @@ const Projectsinfo = [
 
   {
     name: "Color Generator",
-    id: 3,
+    id: 4,
     description:
       "Trying to find the perfect color for your next project? A random color generator app will help you out! This app lets you generate random colors with a few clicks of a button. It's an easy way to get inspired with a splash of bright and bold colors that will surely inspire your next piece of art.",
     github: "https://github.com/ruckyTheWebDesigner/color-generator",
@@ -48,7 +67,7 @@ const Projectsinfo = [
 
   {
     name: "Real Estate app",
-    id: 4,
+    id: 5,
     description:
       "This is a full stack housing marketplace. users can register a profile and list their properties or explore available for rents, The frontend is powered by React and the backend by Firebase firestore.",
     github: "https://github.com/ruckyTheWebDesigner/house-marketplace",
@@ -63,7 +82,7 @@ const Projectsinfo = [
 
   {
     name: "Tours App",
-    id: 5,
+    id: 6,
     description:
       "Discover new places with this app that helps you to find the best tours built with React and Styled Components.",
     github: "https://github.com/ruckyTheWebDesigner/Tours-app",
@@ -76,7 +95,7 @@ const Projectsinfo = [
   },
   {
     name: "Dark Mode Landing Page",
-    id: 6,
+    id: 7,
     description:
       "This is a landing page with button to switch between light and dark mode for better user accessibility.",
     github: "https://github.com/ruckyTheWebDesigner/darkmode-app",
