ca5e83f2dd30c72a2cb8b8ed0ab5b8dc276f4239
ruckyTheWebDesigner <93795260+ruckyTheWebDesigner@users.noreply.github.com>
2022-08-06 13:57:52 +0300

    Added DevJobs App to projects list

--- src/Components/Homepage/Projectsinfo.js
+++ src/Components/Homepage/Projectsinfo.js
@@ -1,10 +1,27 @@
 const Projectsinfo = [
-        {
-    name: "Shortly Url",
+                {
+    name: "DevJobs App",
     id: 1,
 
     description:
-      "Bored of trying to remember websites with long url? Here is a app that can help you to shorten any website url with just a button click.",
+      "Being a developer myself, i understand how fustrating it is to find jobs, here is a Job Portal app to find and filter job search, With dark mode to automatically have the correct color scheme chosen for them based on user device preferences.",
+    url: "https://devjobs-app-sable.vercel.app/",
+            github: 'https://github.com/ruckyTheWebDesigner/devjobs-app',
+    with: {
+      1: "React",
+      2: "MUI prefers-color-scheme",
+      3: "React-router-dom",
+      4: "React-icons",
+    },
+  },
+        
+        
+        {
+    name: "Shortly Url",
+    id: 2,
+
+    description:
+      "Bored of trying to remember websites with long url? Here is a app that can help you to shorten any website url with just a button click. Shortened Url are saved in local session storage so its saved even after page refresh.",
     url: "https://url-api-sigma.vercel.app/",
             github: 'https://github.com/ruckyTheWebDesigner/url-api',
     with: {
@@ -18,10 +35,10 @@ const Projectsinfo = [
     
     {
     name: "Where in the world",
-    id: 2,
+    id: 3,
 
     description:
-      "This is a app that displays all countries with infos, Users can search by country name or select filter, I got the design inspiration from frontend mentor challenges.",
+      "This is a app that displays all countries with infos, Users can search by country name or filter by select dropdown, I got the design inspiration from frontend mentor challenges.",
     url: "https://rest-countries-lfur8553i-ruckythewebdesigner.vercel.app/",
         github: 'https://github.com/ruckyTheWebDesigner/rest-countries',
     with: {
@@ -36,7 +53,7 @@ const Projectsinfo = [
   
   {
     name: "HSA news app",
-    id: 3,
+    id: 4,
 
     description:
       "HSA is a modern, easy to use, and easy to navigate news app. It enables readers to search for and read articles in a split-second. The app uses modern Web technologies like React, Redux, and API Fetch to build it's backend and frontend. ",
@@ -53,7 +70,7 @@ const Projectsinfo = [
 
   {
     name: "Color Generator",
-    id: 4,
+    id: 5,
     description:
       "Trying to find the perfect color for your next project? A random color generator app will help you out! This app lets you generate random colors with a few clicks of a button. It's an easy way to get inspired with a splash of bright and bold colors that will surely inspire your next piece of art.",
     github: "https://github.com/ruckyTheWebDesigner/color-generator",
@@ -67,7 +84,7 @@ const Projectsinfo = [
 
   {
     name: "Real Estate app",
-    id: 5,
+    id: 6,
     description:
       "This is a full stack housing marketplace. users can register a profile and list their properties or explore available for rents, The frontend is powered by React and the backend by Firebase firestore.",
     github: "https://github.com/ruckyTheWebDesigner/house-marketplace",
@@ -82,7 +99,7 @@ const Projectsinfo = [
 
   {
     name: "Tours App",
-    id: 6,
+    id: 7,
     description:
       "Discover new places with this app that helps you to find the best tours built with React and Styled Components.",
     github: "https://github.com/ruckyTheWebDesigner/Tours-app",
@@ -95,7 +112,7 @@ const Projectsinfo = [
   },
   {
     name: "Dark Mode Landing Page",
-    id: 7,
+    id: 8,
     description:
       "This is a landing page with button to switch between light and dark mode for better user accessibility.",
     github: "https://github.com/ruckyTheWebDesigner/darkmode-app",
